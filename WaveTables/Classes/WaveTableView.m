2b1de67abbc0d4f15ff5cd23b45ba0abdedd254e
Rich E <reakinator@gmail.com>
2011-05-21 14:42:16 +1000

    less redrawing with no interpolation

--- WaveTables/Classes/WaveTableView.m
+++ WaveTables/Classes/WaveTableView.m
@@ -60,26 +60,31 @@
 #pragma mark -
 #pragma mark Drawing
 
+static inline CGFloat convertMagToY(CGFloat mag, CGFloat maxHeight) {
+	return (1.0 - mag) * maxHeight * 0.5;
+}
+
 - (void)drawRect:(CGRect)rect {
     CGRect bounds = self.bounds;
 //    DLog(@"bounds: %@, rect: %@", NSStringFromCGRect(bounds), NSStringFromCGRect(rect));
     
     CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetLineWidth(context, 2.0); // TODO: verify it is on an even pixel
-
-    // draw wavetable
+    CGContextSetLineWidth(context, 2.0);
+	
+    // draw the wavetable region that was specified as invalid
     if (self.wavetable) {
         CGContextSetStrokeColorWithColor(context, [self.arrayColor CGColor]);
         
         CGFloat scaleX = bounds.size.width / self.wavetable.length; // the wavetable spans the entire view
-        CGFloat scaleY = bounds.size.height * 0.5; // values in the array are normalized to 1.0
         
-        // TODO: only draw in updated rect
-        CGContextMoveToPoint(context, 0.0, [self.wavetable floatAtIndex:0] * scaleY);
-        for (int i = 0; i < self.wavetable.length; i++) {
-            CGFloat y = (1 - [self.wavetable floatAtIndex:i]) * scaleY;
+		int startX = (int)floor(rect.origin.x / scaleX);
+		int endX = (int)ceil((rect.origin.x + rect.size.width) / scaleX);
+		CGFloat y = convertMagToY([self.wavetable floatAtIndex:startX], bounds.size.height);
+		
+        CGContextMoveToPoint(context, startX * scaleX, y);
+        for (int i = startX + 1; i <= endX; i++) {
+			y = convertMagToY([self.wavetable floatAtIndex:i], bounds.size.height);
             CGContextAddLineToPoint(context, i * scaleX, y);
-//            DLog(@"drawing point at: %f, %f", i * scaleX, y);
         }
         CGContextStrokePath(context);
     }
@@ -116,11 +121,12 @@
 #pragma mark Private
 
 - (void)updateArrayWithPoint:(CGPoint)point {
-	CGFloat	pointSizeInView = (float)self.wavetable.length / self.bounds.size.width;
+	CGSize viewSize = self.bounds.size;
+	CGFloat	pointSizeInView = (float)self.wavetable.length / viewSize.width;
     int index = (int)(point.x * pointSizeInView);
 	int lastIndex = (int)(self.lastPoint.x * pointSizeInView);
 
-	float mag = (point.y * -2.0 / self.bounds.size.height) + 1.0;
+	float mag = (point.y * -2.0 / viewSize.height) + 1.0;
 	int numPoints = abs(lastIndex - index);
 	if (self.dragging && numPoints > 1) {
 		
@@ -141,12 +147,22 @@
 				[self.wavetable setFloat:currentMag atIndex:currentIndex];
 			}
 		}
+		// TODO: only invalidate the section across the points modified
+		[self setNeedsDisplay];
 	} else {
 		// no need to interpolate so just draw one point and store the last calculated value
 		[self.wavetable setFloat:mag atIndex:index];
+		
+		// invalidate a rect that incompasses this index and one index on each side
+		CGFloat sampleSizeInView = ceil(1.0 / pointSizeInView);
+		CGFloat redrawX = (point.x < sampleSizeInView ? 0.0 : point.x - sampleSizeInView);
+		CGFloat redrawWidth = sampleSizeInView * 3.0;
+		
+
+		[self setNeedsDisplayInRect:CGRectMake(redrawX, 0.0, redrawWidth, viewSize.height)];
+
 	}
 	
-	[self setNeedsDisplay];
 	// store the last point so that we can smoothly begin no matter where the next touched point is
 	// instead of storing the y location of the point, store the already calculated mag
     self.lastPoint = CGPointMake(point.x, mag); 
