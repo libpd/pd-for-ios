ef8b6b44a25882dd1efb4df3b598e27eb294a101
Peter Brinkmann <peter.brinkmann@gmail.com>
2011-09-04 00:17:41 -0400

    updated for new utility class

--- DispatcherSample/DispatcherSample/SampleListener.m
+++ DispatcherSample/DispatcherSample/SampleListener.m
@@ -34,7 +34,7 @@
 - (void)receiveFloat:(float)val {
     NSLog(@"Listener %@: float %f\n", label, val);
     NSString *s = [NSString stringWithFormat:@"%f", val];
-    [label performSelectorOnMainThread:@selector(setText:) withObject:s waitUntilDone:NO];
+    [[InvokeOnMainThread withTarget:label] setText:s];
 }
 
 - (void)receiveSymbol:(NSString *)s {
