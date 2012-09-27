6a8e7942fb74c19e9c896abcf86541ab240f6b19
Rich E <reakinator@gmail.com>
2012-09-26 23:07:10 -0400

    removed some unused debug code

--- SlidePadBasic/OtherSources/Common.h
+++ SlidePadBasic/OtherSources/Common.h
@@ -1,14 +1,5 @@
-
-// debug macros from: http://www.cimgf.com/2010/05/02/my-current-prefix-pch-file/
-#ifdef R_DEBUG // TODO: rename this define to R_DEBUG or something
-#define RLog(...) NSLog(@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat:__VA_ARGS__])
-#define ALog(...) [[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding] file:[NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding] lineNumber:__LINE__ description:__VA_ARGS__]
+#ifdef R_DEBUG
+	#define RLog(...) NSLog(@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat:__VA_ARGS__])
 #else
-#define RLog(...) do { } while (0)
-#ifndef NS_BLOCK_ASSERTIONS
-#define NS_BLOCK_ASSERTIONS
+	#define RLog(...) do { } while (0)
 #endif
-#define ALog(...) NSLog(@"%s %@", __PRETTY_FUNCTION__, [NSString stringWithFormat:__VA_ARGS__])
-#endif
-
-#define ZAssert(condition, ...) do { if (!(condition)) { ALog(__VA_ARGS__); }} while(0)
